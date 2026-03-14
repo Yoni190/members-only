d2c75d390cc7148574c4aee252ae4c84c4e0c7d1
Yoni190 <yfisuhu@gmail.com>
2026-03-14 14:09:32 +0300

    Add membership column in the query

--- db/queries.js
+++ db/queries.js
@@ -6,7 +6,7 @@ async function register(f_name, l_name, username, password) {
     try {
         const hashedPassword = bcrypt.hash(password, 10)
 
-        await pool.query('INSERT INTO users (f_name, l_name, username, password) VALUES ($1, $2, $3, $4)', [f_name, l_name, username, hashedPassword])
+        await pool.query('INSERT INTO users (f_name, l_name, username, password, membership) VALUES ($1, $2, $3, $4, $5)', [f_name, l_name, username, hashedPassword, false])
     } catch (error) {
         console.error(error)
     }
