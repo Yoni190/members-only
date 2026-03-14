696fd6b6851a291ecb968b3cdc794e065545a85b
Yoni190 <yfisuhu@gmail.com>
2026-03-14 15:01:41 +0300

    Add custom validation to check already register username

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,7 +8,14 @@ const validate = [
         .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long.'),
     body('confirm_password').custom((value, { req }) => {
         return value === req.body.password
-    }).withMessage('Password and Confirm password must be the same')
+    }).withMessage('Password and Confirm password must be the same'),
+    body('username').custom((async value => {
+        const user = await db.checkUsername(value)
+        console.log(user.length > 0)
+        if(user.length > 0) {
+            throw new Error('Username already registered!')
+        }
+    }))
 ]
 
 exports.register = [
@@ -24,10 +31,6 @@ exports.register = [
 
         const { f_name, l_name, username, password } = matchedData(req)
 
-        const user = await db.checkUsername(username)
-        if(user.length > 0) {
-            return res.status(400).json({ message: 'There already exists a user with this username! '})
-        }
 
         const hashedPassword = await bcrypt.hash(password, 10)
 
@@ -36,18 +39,3 @@ exports.register = [
         res.status(200).json({ message: 'User registered successfully.' })
     }
 ]
-
-// async function register(req, res, next) {
-//     const { f_name, l_name, username, password } = req.body
-
-//     const user = await db.checkUsername(username)
-//     if(user.length > 0) {
-//         return res.status(400).json({ message: 'There already exists a user with this username! '})
-//     }
-
-//     const hashedPassword = await bcrypt.hash(password, 10)
-
-//     await db.register(f_name, l_name, username, hashedPassword)
-
-//     res.status(200).json({ message: 'User registered successfully.' })
-// }
