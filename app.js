f887cb59c4b0fb2ddd3e443ff96ce5a61319767c
Yoni190 <yfisuhu@gmail.com>
2026-03-14 14:28:07 +0300

    Setup views configuration

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const authRoute = require('./routes/authRoute')
+const path = require('node:path')
 require('dotenv').config()
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(authRoute)
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 const PORT = process.env.PORT || 3000
 
