66c50495adc0614c448f4f26e984158d0775193a
Yoni190 <yfisuhu@gmail.com>
2026-03-14 13:57:45 +0300

    Setup app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, (error) => {
+    if(error) {
+        throw error
+    }
+
+    console.log(`Server listening on port ${PORT}`)
+})
