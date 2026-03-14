02a478d2ebec6f958bd7d24439f2c9889ac688f2
Yoni190 <yfisuhu@gmail.com>
2026-03-14 14:50:49 +0300

    Add validation for password length

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,22 +1,51 @@
 const db = require('../db/queries')
 const bcrypt = require('bcrypt')
+const { body, validationResult, matchedData } = require('express-validator')
 
 
-async function register(req, res, next) {
-    const { f_name, l_name, username, password } = req.body
+const validate = [
+    body('password').trim()
+        .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long.'),
+    
+]
 
-    const user = await db.checkUsername(username)
-    if(user.length > 0) {
-        return res.status(400).json({ message: 'There already exists a user with this username! '})
+exports.register = [
+    validate,
+    async (req, res) => {
+        const errors = validationResult(req)
+
+        if(!errors.isEmpty()) {
+            return res.status(400).render('sign-up', {
+                errors: errors.array()
+            })
+        }
+
+        const { f_name, l_name, username, password } = matchedData(req)
+
+        const user = await db.checkUsername(username)
+        if(user.length > 0) {
+            return res.status(400).json({ message: 'There already exists a user with this username! '})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        await db.register(f_name, l_name, username, hashedPassword)
+
+        res.status(200).json({ message: 'User registered successfully.' })
     }
+]
 
-    const hashedPassword = await bcrypt.hash(password, 10)
+// async function register(req, res, next) {
+//     const { f_name, l_name, username, password } = req.body
 
-    await db.register(f_name, l_name, username, hashedPassword)
+//     const user = await db.checkUsername(username)
+//     if(user.length > 0) {
+//         return res.status(400).json({ message: 'There already exists a user with this username! '})
+//     }
 
-    res.status(200).json({ message: 'User registered successfully.' })
-}
+//     const hashedPassword = await bcrypt.hash(password, 10)
 
-module.exports = {
-    register
-}
+//     await db.register(f_name, l_name, username, hashedPassword)
+
+//     res.status(200).json({ message: 'User registered successfully.' })
+// }
