b0df71c2779c7cb2ec00c89d577b8e2696ebefda
Yoni190 <yfisuhu@gmail.com>
2026-03-14 14:17:53 +0300

    Use authRoute in app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const authRoute = require('./routes/authRoute')
 require('dotenv').config()
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(authRoute)
 
 const PORT = process.env.PORT || 3000
 
