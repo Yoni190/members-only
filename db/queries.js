8d0c4595b5594ef02c7a0b862f651f10be19a107
Yoni190 <yfisuhu@gmail.com>
2026-03-14 14:17:14 +0300

    Movie encryption to controller

--- db/queries.js
+++ db/queries.js
@@ -1,12 +1,9 @@
 const pool = require('./pool')
-const bcrypt = require('bcrypt')
 
 
 async function register(f_name, l_name, username, password) {
     try {
-        const hashedPassword = bcrypt.hash(password, 10)
-
-        await pool.query('INSERT INTO users (f_name, l_name, username, password, membership) VALUES ($1, $2, $3, $4, $5)', [f_name, l_name, username, hashedPassword, false])
+        await pool.query('INSERT INTO users (f_name, l_name, username, password, membership) VALUES ($1, $2, $3, $4, $5)', [f_name, l_name, username, password, false])
     } catch (error) {
         console.error(error)
     }
