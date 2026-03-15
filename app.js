f3c5b07f2cb8604263efc09266f6b792810a84c6
Yoni190 <yfisuhu@gmail.com>
2026-03-15 14:46:58 +0300

    Config passport.js

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ const express = require('express')
 const authRoute = require('./routes/authRoute')
 const userRoute = require('./routes/userRoute')
 const path = require('node:path')
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
 require('dotenv').config()
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
 app.use(authRoute)
 app.use(userRoute)
+
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false
+    })
+)
+app.use(passport.session())
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
