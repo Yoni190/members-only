024d92ed152a236404cb914be51d8a7f9b60652a
Yoni190 <yfisuhu@gmail.com>
2026-03-15 20:50:53 +0300

    Show home to unauthenticated users

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,10 +4,10 @@ const { isAuth } = require('../middlewares/authMiddleware')
 
 const router = Router()
 
-router.get(['/', '/register'], userController.registerView)
+router.get('/register', userController.registerView)
 router.get('/login', userController.loginView)
+router.get('/', userController.home)
 
-router.get('/home', isAuth, userController.home)
 router.get('/account', isAuth, userController.account)
 router.get('/activate-membership', isAuth, userController.activateView)
 router.post('/activate-membership', isAuth, userController.activate)
