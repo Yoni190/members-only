176e20d495a98058b310a92ca89448b555d619cd
Yoni190 <yfisuhu@gmail.com>
2026-03-15 19:25:05 +0300

    Validate createPost data

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,19 +1,37 @@
 const db = require('../db/queries')
-
-function createView(req, res) {
-    res.render('create-post')
-}
-
-async function create(req, res) {
-    const { title, message } = req.body
-    const user_id = req.session.passport.user
-
-    await db.createPost(title, message, user_id)
-    res.redirect('/home')
-}
+const { body, validationResult, matchedData } = require('express-validator')
 
 
-module.exports = {
-    createView,
-    create
-}
+const validatePost = [
+    body('title').trim()
+        .notEmpty().withMessage('Please enter post title'),
+    body('message').trim()
+        .notEmpty().withMessage('Please enter post message')
+]
+
+exports.createView = [
+    (req, res) => {
+        res.render('create-post')
+    }
+]
+
+exports.create = [
+    validatePost,
+    async (req, res) => {
+        const errors = validationResult(req)
+
+        if(!errors.isEmpty()) {
+            return res.status(400).render('create-post', {
+                errors: errors.array()
+            })
+        }
+
+        const { title, message } = matchedData(req)
+
+        const user_id = req.session.passport.user
+
+        await db.createPost(title, message, user_id)
+        res.redirect('/home')
+    }
+]
+
