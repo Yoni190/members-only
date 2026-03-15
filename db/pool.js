6bb87a077f0669f581e7e63f77c5c49b5503c8a0
Yoni190 <yfisuhu@gmail.com>
2026-03-15 23:54:18 +0300

    Setup pool using connection string

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,15 @@
 const { Pool } = require('pg')
+const { connectionString } = require('pg/lib/defaults')
 require('dotenv').config()
 
+// module.exports = new Pool({
+//     host: process.env.HOST,
+//     user: process.env.ROLENAME,
+//     database: process.env.DBNAME,
+//     password: process.env.ROLEPASSWORD,
+//     port: process.env.DBPORT
+// })
+
 module.exports = new Pool({
-    host: process.env.HOST,
-    user: process.env.ROLENAME,
-    database: process.env.DBNAME,
-    password: process.env.ROLEPASSWORD,
-    port: process.env.DBPORT
+    connectionString: `postgresql://${process.env.ROLENAME}:${process.env.ROLEPASSWORD}@${process.env.HOST}/${process.env.DBNAME}?sslmode=${process.env.PGSSLMODE}&channel_binding=${process.env.PGCHANNELBINDING}`
 })
