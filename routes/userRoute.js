8ac680e4385666cce0fcd886582ba1bded8f084c
Yoni190 <yfisuhu@gmail.com>
2026-03-15 14:42:03 +0300

    Create route to get login view

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/userController')
 const router = Router()
 
 router.get('/register', userController.registerView)
+router.get('/login', userController.loginView)
 
 module.exports = router
