e6cc1b4905e35edf0eb8e16662f27e2a786966c1
Yoni190 <yfisuhu@gmail.com>
2026-03-15 20:45:15 +0300

    Create route to delete posts

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 const adminController = require('../controllers/adminController')
 const { isAuth } = require('../middlewares/authMiddleware')
+const { isAdmin } = require('../middlewares/adminMiddleware')
 
 const router = Router()
 
 router.get('/admin', adminController.adminView)
 router.post('/admin', adminController.setAdmin)
+router.post('/delete-post/:id', isAuth, isAdmin, adminController.deletePost)
 
 module.exports = router
